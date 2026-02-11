3228c03764c4dc310cf8aa291908f7a82b168218
Tate <tatele13@gmail.com>
2026-02-10 22:02:47 -0600

    Send Message section works, contact info section updated

--- js/main.js
+++ js/main.js
@@ -14,4 +14,29 @@ $(document).ready(function () {
       );
     }
   });
+
+  $(".contactForm").on("submit", function (e) {
+    e.preventDefault();
+
+    const form = $(this);
+
+    $.ajax({
+      url: form.attr("action"),
+      method: form.attr("method"),
+      data: form.serialize(),
+      dataType: "json",
+      success: function () {
+        $("#toast").stop(true, true).slideDown(300);
+
+        form[0].reset();
+
+        setTimeout(function () {
+          $("#toast").slideUp(300);
+        }, 3000);
+      },
+      error: function () {
+        alert("Something went wrong. Please try again.");
+      },
+    });
+  });
 });
